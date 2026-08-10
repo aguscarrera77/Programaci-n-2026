28bdf7709f3a7142c1d57e004b1d15d34a546c0a
aguscarrera77 <aguscarrera77@gmail.com>
2026-08-10 20:24:09 -0300

    10/8

--- Javascript/carrera.js
+++ Javascript/carrera.js
@@ -16,23 +16,34 @@
 
 //console.log("Cantidad de vueltas: " + cantidadVueltas)
 
-function carrera(){
-    let combustible=60;
-    let cantidadVueltas=0;
-    let autoSano=true;
-    while(combustible>0 && autoSano){
-        combustible -=15;
-        cantidadVueltas ++;
+function simuladorCarrera(){
 
-        let pregunta=prompt("Chocaste en la vuelta?" +cantidadVueltas+"si/no");
-        if(pregunta===si){
-            autoSano=false;
-        }
-        return cantidadVueltas;
+let combustible=60;
+let cantidadVueltas=0;
+let autoSano=true;
+while(autoSano && combustible > 0 ){
+cantidadVueltas++;
+combustible -=15;
 
-    }
+let respuesta= prompt("Chocaste: (si/no)".toLowerCase())
+
+if(!(respuesta==="no"))
+{
+    autoSano=false;
 }
 
-function mostrarReporte(carrera){
 
 }
+
+console.log("-------------------------------------------------");
+console.log("Cantidad de vueltas: "+ cantidadVueltas);
+
+if(!autoSano){
+    console.log("La carrera termino porque chocaste.")
+}
+else {console.log("La carrera termino porque se quedo sin combustible.")}
+
+
+}
+
+simuladorCarrera();
--- Javascript/funciones.js
+++ Javascript/funciones.js
@@ -3,7 +3,7 @@
 // codigo;
 //     return(retorna el valor adentrod de la funcion.)}
 
-function sumar (a,b){
+/*function sumar (a,b){
     return a + b;
 }
 console.log(sumar(10,4))
@@ -24,4 +24,26 @@ function precioFinal(precio,descuento){
 
 
 let precio=precioFinal(prompt())
-console.log(precio)
+console.log(precio)*/
+
+/* CARA O CRUZ*/
+
+function lanzarMoneda(eleccionTirada){
+/*Variable de la tirada de la moneda*/
+
+const tirada=Math.random();
+let resultadoTirada="";
+
+if(tirada<0.5){
+    resultadoTirada="cara";
+}
+else{resultadoTirada="cruz"}
+
+if(eleccionTirada === resultadoTirada){
+    return "Ganaste: salio " + resultadoTirada;
+}
+else{return "Perdiste: salio" + resultadoTirada}
+
+}
+
+console.log(lanzarMoneda("cara"))
