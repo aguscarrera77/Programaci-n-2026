2dd5bb7c0a57b4f221bf0ea73c08c6e2c02c5757
aguscarrera77 <aguscarrera77@gmail.com>
2026-09-09 20:10:33 -0300

    9/9

--- Javascript/menuHamburguesa.js
+++ Javascript/menuHamburguesa.js
@@ -1,1 +1,18 @@
-alert(hola) 
+const submenu=document.querySelector("#submenu");
+const submenuTrigger=document.querySelector("#submenu-trigger");
+const submenuItem=document.querySelector('#submenu-Item');
+
+submenuTrigger.addEventListener("click",function(e){
+    e.preventDefault();//evita que el enlace recargue la pagina.
+    submenu.classList.toggle("active");
+});
+submenuItem.addEventListener('mouseleave',function(){
+    submenu.classList.remove("active");
+});
+
+//agregamos una variable que distinga entre el click simple y doble click.
+
+let clickTimer;
+
+
+
