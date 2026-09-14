873eb1857b41f5584398ec013fe028afe69a436a
aguscarrera77 <aguscarrera77@gmail.com>
2026-09-14 20:48:34 -0300

    14/9

--- Javascript/menuHamburguesa.js
+++ Javascript/menuHamburguesa.js
@@ -12,7 +12,58 @@ submenuItem.addEventListener('mouseleave',function(){
 
 //agregamos una variable que distinga entre el click simple y doble click.
 
-let clickTimer;
+const botonHamburguesa=document.querySelector('#btn-hamburguesa');
+const navMovil=document.querySelector('#navMovil');
+const submenu2=document.querySelector('#submenu2');
+const submenuMovil=document.querySelector('#btn-submenuMovil');
+
+// entre al dom y llame a todos las etiquetas que contiene el boton, menu y submenu.
+
+botonHamburguesa.addEventListener("click",function(){
+navMovil.classList.toogle('abierto');
+});
+//doble para cerrar el boton. Cierra todo.
+botonHamburguesa.addEventListener('dblclick',function(){
+navMovil.classList.remove('abierto');
+submenu2.classList.remove('activo');
 
 
+})
+//click del submenu
 
+submenuMovil.addEventListener('click',function(event){
+    event.preventDefault();
+    submenu2.classList.toogle('activo');
+
+})
+  //-------------- Agregar productos haciendo click en el boton
+
+  const formProducto=document.querySelector('#formProducto');
+
+  const nombreProducto=document.querySelector("#nombreProducto");
+  const precioProducto=document.querySelector("#precioProducto");
+  const listaProducto=document.querySelector("#listaProducto");
+
+  const productos=[];
+
+  formProducto.addEventListener('submit',function(e){
+    e.preventDefault();
+    const nombre=nombreProducto.value.trim();
+    const precio=parseFloat(precioProducto.value);
+    if(nombre==="" || isNaN(precio))return;
+    const nuevoproducto={
+        id:Date.now(),// generar un id de acuerdo la fecha.
+        nombre:nombre,
+        precio:precio
+
+    };
+    productos.push(nuevoproducto);
+
+    agregarProductoalDom(nuevoproducto);
+    formProducto.reset();
+
+  });
+
+  function agregarProductoalDom(nuevoproducto){
+
+  }
